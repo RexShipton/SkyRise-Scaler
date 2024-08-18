43be40ec2d33cb048f90d9a741fde15d0d0ecf41
shipx7 <132112324+shipx7@users.noreply.github.com>
2024-08-18 02:15:43 -0500

    character finished except crouch animations

--- new-game-project/scenes/player.gd
+++ new-game-project/scenes/player.gd
@@ -1,29 +1,81 @@
 extends CharacterBody2D
 
+
+
+var jump_buffer : bool = false
+var jump_available : bool = true
+
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var jump_buffer_timer: Timer = $JumpBufferTimer
+@onready var coyote_timer: Timer = $CoyoteTimer
+
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
+@export var Jump_Buffer_Time : float = .1
+@export var Coyote_Time: float = .1
 
 
-var is_in_air
-
+func _ready() -> void:
+	jump_buffer_timer.wait_time = Jump_Buffer_Time
+	coyote_timer.wait_time = Coyote_Time
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		if jump_available and coyote_timer.is_stopped():
+			coyote_timer.start()
+		animation_player.play("airborne")
 		velocity += get_gravity() * delta
-
+		
+	if is_on_floor() :
+		jump_available = true
+		if jump_buffer:
+			Jump()
+		if Input.get_action_strength("character_crouch") > 0:
+			if scale.y < -.9 or scale.y > .9:
+				scale.y = scale.y / 1.5
+		if Input.get_action_strength("character_crouch") == 0 and scale.y > -.9 and scale.y < .9:
+			print("hit")
+			scale.y = scale.y * 1.5
+		if Input.get_axis("character_left", "character_right") == 0:
+			animation_player.play("idle")
+		if Input.get_axis("character_left", "character_right") != 0:
+			animation_player.play("run")
+		
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("character_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("character_jump"):
+		if jump_available:
+			Jump()
+		else:
+			jump_buffer = true
+			jump_buffer_timer.start()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("character_left", "character_right")
 	if direction:
+		if self.velocity.x < 0:
+			self.scale.y = abs(self.scale.y) * -1
+			self.rotation_degrees = 180
+#		parent.Sprite.flip_h = true
+		if self.velocity.x > 0:
+			self.scale.y = abs(self.scale.y)
+			self.rotation_degrees = 0
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+func Jump():
+	velocity.y = JUMP_VELOCITY
+	jump_buffer = false
+	jump_available = false
+
+func _on_jump_buffer_timer_timeout() -> void:
+	jump_buffer = false
+
+func _on_coyote_timer_timeout() -> void:
+	jump_available = false
