c2f7adb1725f345f950522730dec96bd5918eb79
BeardedGamer5 <132113578+BeardedGamer5@users.noreply.github.com>
2024-08-18 16:53:25 -0500

    Added high score tracker resource.

--- new-game-project/scenes/hud.gd
+++ new-game-project/scenes/hud.gd
@@ -3,21 +3,22 @@ extends CanvasLayer
 @onready var score_label: Label = $TopRight/Panel/MarginContainer/VBoxContainer/HBoxContainer2/ScoreLabel
 @onready var game_over_score_label: Label = $Center/GameOverPanel/MarginContainer/VBoxContainer/HBoxContainer/GameOverScoreLabel
 @onready var game_over_panel: Panel = $Center/GameOverPanel
+var SCORE_RESOURCE = preload("res://ScoreResource.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func update_score(value : float) -> void:
-	var score_text = str(value) + " feet!"
+	var score_text = str(SCORE_RESOURCE.score) + " feet!"
 	score_label.text = score_text
 	game_over_score_label.text = score_text
 
-func update_tile(tile : int) -> void:
+func update_tile(_tile : int) -> void:
 	# This would be dependent on some stuff.
 	pass
 
--- new-game-project/scenes/main_menu.gd
+++ new-game-project/scenes/main_menu.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
+@onready var high_score_label: Label = $Top/Panel/MarginContainer/HBoxContainer/HighScoreLabel
+var SCORE_RESOURCE = preload("res://ScoreResource.tres")
 
+func _ready() -> void:
+	high_score_label.text = str(SCORE_RESOURCE.score) + " feet!"
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
