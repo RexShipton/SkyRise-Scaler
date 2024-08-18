626a5e2690e6f4fc0186e418ffb19f801003faff
BeardedGamer5 <132113578+BeardedGamer5@users.noreply.github.com>
2024-08-18 03:11:39 -0500

    Added Game Over panel to hud.

--- new-game-project/scenes/hud.gd
+++ new-game-project/scenes/hud.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
-@onready var score_label: Label = $TopRight/Panel/VBoxContainer/HBoxContainer2/ScoreLabel
+@onready var score_label: Label = $TopRight/Panel/MarginContainer/VBoxContainer/HBoxContainer2/ScoreLabel
+@onready var game_over_score_label: Label = $Center/GameOverPanel/MarginContainer/VBoxContainer/HBoxContainer/GameOverScoreLabel
+@onready var game_over_panel: Panel = $Center/GameOverPanel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +13,17 @@ func _process(delta: float) -> void:
 	pass
 
 func update_score(value : float) -> void:
-	score_label.text = str(value) + " feet!"
+	var score_text = str(value) + " feet!"
+	score_label.text = score_text
+	game_over_score_label.text = score_text
 
 func update_tile(tile : int) -> void:
 	# This would be dependent on some stuff.
 	pass
+
+func game_over() -> void:
+	game_over_panel.visible = true
+
+func _on_game_over_main_menu_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
